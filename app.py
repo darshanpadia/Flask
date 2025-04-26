01d1f81bc91faaaa38cf41c2a182b524726985c2
darshanpadia <darshan.padia@somaiya.edu>
2025-04-26 21:08:04 +0530

    added get_dog_breeds and dashboard template

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, Response, jsonify, render_template
+import json
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello"
+
+@app.route("/get_dog_breeds", methods=['GET'])
+def get_dog_breeds():
+    response = requests.get('https://dog.ceo/api/breeds/list/all')
+    json_res = json.loads(response.text)
+    breeds = json_res["message"]
+    return render_template("/dashboard.html", breeds=breeds)
+
+if __name__ == '__main__':
+    app.run()
+
